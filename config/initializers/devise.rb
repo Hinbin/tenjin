d342926c94605aff74c8be41ae710df7885edef1
Hinbin <nicholashoulton@gmail.com>
2019-04-25 10:55:02 +0100

    Configure Amazon S3

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   callback_url = 'http://localhost:3000/users/auth/wonde/callback'
 
   if Rails.env == 'production'
-    callback_url = 'https://protected-meadow-45841.herokuapp.com/users/auth/wonde/callback'
+    callback_url = 'https://tenjin.herokuapp.com/users/auth/wonde/callback'
   end
 
   config.omniauth :wonde, 
