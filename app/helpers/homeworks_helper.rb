831f65b3ceb033d72a5aa90802d820ee883905dd
Hinbin <nicholashoulton@gmail.com>
2019-04-26 08:05:19 +0100

    Change production database to hobby dev

--- app/helpers/homeworks_helper.rb
+++ app/helpers/homeworks_helper.rb
@@ -1,10 +1,8 @@
 module HomeworksHelper
-
   def report_progress(homework_progress)
     completed = homework_progress.where(completed: true).count
     number_of_students = homework_progress.count
-    ( completed.to_s + " / " + number_of_students.to_s ) + 
-    " - " + number_to_percentage( (completed.to_f / number_of_students.to_f) * 100, precision: 0 )
+    (completed.to_s + ' / ' + number_of_students.to_s) +
+      ' - ' + number_to_percentage((completed.to_f / number_of_students.to_f) * 100, precision: 0)
   end
-  
 end
