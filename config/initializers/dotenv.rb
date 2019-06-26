97ee12d108b4a170fd04790c0c7c91f1751ccdbd
Hinbin <nicholashoulton@gmail.com>
2019-06-26 11:54:19 +0100

    Only check keys in dev or test

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,3 +1,5 @@
-Dotenv.require_keys("WONDE_SECRET", "WONDE_CLIENT_ID", "WONDE_CALLBACK_URL",
-                    "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"
-                  )
+if ENV['RAILS_ENV'] = "development" || "test"
+  Dotenv.require_keys("WONDE_SECRET", "WONDE_CLIENT_ID", "WONDE_CALLBACK_URL", # Wonde ENV vars
+                      "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"             # AWS for ActiveStorage ENV
+                    )
+end
