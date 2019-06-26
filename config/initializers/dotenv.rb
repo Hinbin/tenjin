34a7e207a6c4b0d262f946abb4671a7a06e604d2
Hinbin <nicholashoulton@gmail.com>
2019-06-26 11:59:43 +0100

    Remove environment variable check

--- config/initializers/dotenv.rb
+++
@@ -1,5 +0,0 @@
-if ENV['RAILS_ENV'] = "development" || "test"
-  Dotenv.require_keys("WONDE_SECRET", "WONDE_CLIENT_ID", "WONDE_CALLBACK_URL", # Wonde ENV vars
-                      "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"             # AWS for ActiveStorage ENV
-                    )
-end
