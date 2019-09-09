94daebd1801f8c68a9db5c9e06631d4c06e3648d
Hinbin <nicholashoulton@gmail.com>
2019-09-09 19:26:14 +0100

    Tweak Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -31,5 +31,9 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 #
 preload_app!
 
+# Start Barnes to support Heroku runtime metrics.
+#
+before_fork { Barnes.start }
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
