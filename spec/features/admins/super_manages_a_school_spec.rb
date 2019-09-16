82bf3b5eed8c5a4d44e0916f8bd95fd32ff1e444
Hinbin <nicholashoulton@gmail.com>
2019-09-16 10:23:39 +0100

    Admin login fix

--- spec/features/admins/super_manages_a_school_spec.rb
+++ spec/features/admins/super_manages_a_school_spec.rb
@@ -1,10 +1,22 @@
 RSpec.describe 'Super manages a school', type: :feature, js: true, default_creates: true do
+  let(:admin_password) { FFaker::Lorem.word }
+
   before do
-    sign_in super_admin
+    super_admin
+    super_admin.reset_password(admin_password, admin_password)
+  end
+
+  it 'allows an admin to log in' do
+    visit(new_admin_session_path)
+    fill_in 'Email', with: super_admin.email
+    fill_in 'Password', with: admin_password
+    click_button 'Log in'
+    expect(page).to have_content('Schools')
   end
 
   context 'when becoming a school admin' do
     before do
+      sign_in super_admin
       school_admin
     end
 
@@ -17,6 +29,7 @@ RSpec.describe 'Super manages a school', type: :feature, js: true, default_creat
 
   context 'when assigning school admins to a school' do
     before do
+      sign_in super_admin
       teacher
       school_admin
     end
