374b2b94ed99be9143d62eaacd6f9340fa8f47c0
Hinbin <nicholashoulton@gmail.com>
2019-10-18 13:11:33 +0100

    Adjust classroom count on migration

--- db/migrate/20191017205758_add_classroom_counter_cache.rb
+++ db/migrate/20191017205758_add_classroom_counter_cache.rb
@@ -1,5 +1,8 @@
 class AddClassroomCounterCache < ActiveRecord::Migration[6.0]
   def change
     add_column :classrooms, :enrollments_count, :integer
+    Classroom.find_each do |classroom|
+      Classroom.reset_counters(classroom.id, :enrollments)
+    end
   end
 end
