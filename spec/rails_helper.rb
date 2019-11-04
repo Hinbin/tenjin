b797aab00beab46a82094a54deb63b75e7bb291e
Hinbin <nicholashoulton@gmail.com>
2019-11-04 19:53:53 +0000

    Added circleci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -158,7 +158,7 @@ RSpec.configure do |config|
     end
   else
     config.before(:each, type: :system) do
-      driven_by :selenium_chrome
+      driven_by :selenium_chrome_headless
     end
   end
 end
