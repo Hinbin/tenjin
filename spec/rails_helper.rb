bfe2c3e8226047f5cf305b1c03a37913ac37ab23
Tom Crouch <tom.crouch@gmail.com>
2019-11-12 21:17:56 +0000

    Fix spring

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# Start code coverage
-require 'simplecov'
-SimpleCov.start 'rails'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
