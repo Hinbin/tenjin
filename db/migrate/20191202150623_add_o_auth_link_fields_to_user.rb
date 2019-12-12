07e05696ac297ca8af63c79d4223ea41db6c8c4d
Hinbin <nicholashoulton@gmail.com>
2019-12-12 09:56:15 +0000

    Updated oAuth default values

--- db/migrate/20191202150623_add_o_auth_link_fields_to_user.rb
+++ db/migrate/20191202150623_add_o_auth_link_fields_to_user.rb
@@ -1,7 +1,7 @@
 class AddOAuthLinkFieldsToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :oauth_provider, :string, default: true
-    add_column :users, :oauth_uid, :string, default: true
-    add_column :users, :oauth_email, :string, default: true
+    add_column :users, :oauth_provider, :string
+    add_column :users, :oauth_uid, :string
+    add_column :users, :oauth_email, :string
   end
 end
