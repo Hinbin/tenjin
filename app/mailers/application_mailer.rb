054f5eef3fb5e241e678b82acb652b3ccb3cd8ca
Hinbin <nicholashoulton@gmail.com>
2020-01-28 14:23:33 +0000

    DEFAULT_EMAIL_FROM ENV for application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'tenjin@outwood.com'
+  default from: ENV['DEFAULT_EMAIL_FROM']
   layout 'mailer'
 end
