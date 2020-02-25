811f5363f3b882d258502948f3d3171bf3a109fb
Hinbin <nicholashoulton@gmail.com>
2020-02-25 17:57:00 +0000

    Fixed tests

--- spec/system/leaderboard/user_views_an_updating_leaderboard_spec.rb
+++ spec/system/leaderboard/user_views_an_updating_leaderboard_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 require 'support/api_data'
 require 'pry'
 
-RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
+RSpec.describe 'User views an updating leaderboard',  type: :system, js: true do
   include_context 'default_creates'
 
   let(:new_entry) { create(:topic_score, topic: topic, school: school, score: 11) }
@@ -23,24 +23,24 @@ RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
     end
 
     it 'flashes an update if I have a score' do
-      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, student_topic_score.user).call
       expect(page).to have_css('tr#row-' + student.id.to_s + '.score-changed')
     end
 
     it 'flashes an update if someone else has a score' do
-      Leaderboard::BroadcastLeaderboardPoint.new(TopicScore.where(user_id: User.second).first).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, User.second).call
       expect(page).to have_css("tr#row-#{User.second.id}.score-changed")
     end
 
     it 'receives two scores at the same time for different users' do
-      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score).call
-      Leaderboard::BroadcastLeaderboardPoint.new(TopicScore.where(user_id: User.second).first).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, student).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, User.second).call
       expect(page).to have_css("tr#row-#{student.id}.score-changed")
         .and have_css("tr#row-#{User.second.id}.score-changed")
     end
 
     it 'only displays 10 users after adding a new person' do
-      Leaderboard::BroadcastLeaderboardPoint.new(new_entry).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, new_entry.user).call
       expect(page).to have_no_css('tr:nth-child(11)')
     end
 
@@ -50,12 +50,13 @@ RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
     end
 
     it 'only flashes once, for a second' do
-      Leaderboard::BroadcastLeaderboardPoint.new(new_entry).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, new_entry.user).call
       expect(page).to have_no_css('tr.score-changed', wait: 1.5)
     end
 
     it 're-ranks correctly' do
-      Leaderboard::BroadcastLeaderboardPoint.new(new_entry).call
+      new_entry
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, new_entry.user).call      
       expect(page).to have_css('tr:nth-child(2)#row-' + student.id.to_s)
     end
   end
@@ -71,25 +72,27 @@ RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
     let(:student_another_school) { create(:student) }
     let(:student_same_school_group) { create(:student, school_group: student.school.school_group) }
     let(:topic_score_same_school_group) { create(:topic_score, topic: topic, school: second_school) }
+    let(:topic_score_different_school_group) { create(:topic_score, topic: topic) }
     let(:another_name) { initialize_name User.second }
 
-    it 'does not update if update is from an unaffiliated school group' do
-      Leaderboard::BroadcastLeaderboardPoint.new(create(:topic_score, topic: topic)).call
+    it 'does not update if update is from an differnt school group' do
+      topic_score_different_school_group
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, student_another_school).call
       expect(page).to have_no_css('tr.score-changed')
     end
 
     it 'displays a new student with a score in the correct window' do
-      Leaderboard::BroadcastLeaderboardPoint.new(new_entry).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, new_entry.user).call
       expect(page).to have_css('tr#row-' + new_entry.user_id.to_s + '.score-changed')
     end
 
     it 'displays the name of a new student with a score ' do
-      Leaderboard::BroadcastLeaderboardPoint.new(new_entry).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, new_entry.user).call
       expect(page).to have_css('tr#row-' + new_entry.user_id.to_s, text: new_entry.user.forename)
     end
 
     it 'shows updates from only my school by default' do
-      Leaderboard::BroadcastLeaderboardPoint.new(topic_score_same_school_group).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic, topic_score_same_school_group.user).call
       expect(page).to have_no_css('td',
                                   exact_text: "#{topic_score_same_school_group.user.forename} #{topic_score_same_school_group.user.surname[0]}")
     end
@@ -97,7 +100,7 @@ RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
     it 'updates if score is from the same school group' do
       click_button('Select School')
       click_button('All')
-      Leaderboard::BroadcastLeaderboardPoint.new(topic_score_same_school_group).call
+      Leaderboard::BroadcastLeaderboardPoint.new(topic_score_same_school_group.topic, topic_score_same_school_group.user).call
       expect(page).to have_css('tr.score-changed')
     end
   end
@@ -111,12 +114,12 @@ RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
     end
 
     it 'updates if someone from the same schools has a score' do
-      Leaderboard::BroadcastLeaderboardPoint.new(TopicScore.second).call
+      Leaderboard::BroadcastLeaderboardPoint.new(TopicScore.second, User.second).call
       expect(page).to have_css("tr#row-#{TopicScore.second.user.id}.score-changed")
     end
 
     it 'updates me when I have a score' do
-      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score).call
+      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score, student_topic_score.user).call
       expect(page).to have_css("tr#row-#{student.id}.score-changed")
     end
   end
@@ -132,7 +135,7 @@ RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
     let(:different_subject_score) { create(:topic_score, school: school, score: 11, subject: different_subject) }
 
     it 'does not update for a different subject' do
-      Leaderboard::BroadcastLeaderboardPoint.new(different_subject_score).call
+      Leaderboard::BroadcastLeaderboardPoint.new(different_subject_score, different_subject_score.user).call
       expect(page).to have_no_css('tr.score-changed')
     end
   end
@@ -148,18 +151,18 @@ RSpec.describe 'User views an updating leaderboard', type: :system, js: true do
     let(:different_topic_score) { create(:topic_score, school: school, score: 11, topic: different_topic) }
 
     it 'updates for the current topic' do
-      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score).call
+      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score, student_topic_score.user).call
       expect(page).to have_css('tr.score-changed')
     end
 
     it 'updates with the topic score and not the total score' do
       different_topic_score
-      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score).call
+      Leaderboard::BroadcastLeaderboardPoint.new(student_topic_score, student_topic_score.user).call
       expect(page).to have_css('td', exact_text: student_topic_score.score)
     end
 
     it 'does not update for a different topic' do
-      Leaderboard::BroadcastLeaderboardPoint.new(different_topic_score).call
+      Leaderboard::BroadcastLeaderboardPoint.new(different_topic_score, different_topic_score.user).call
       expect(page).to have_no_css('tr.score-changed')
     end
   end
