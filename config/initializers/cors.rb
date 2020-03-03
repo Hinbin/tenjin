13dcb39a4d7caa41307f73c9811d535ae926c025
Tom Crouch <tom.crouch@gmail.com>
2020-03-03 19:39:40 +0000

    Add rack-cors gem

---
+++ config/initializers/cors.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Configure Rack::Cors middleware
+#
+# Add CORS headers when serving assets
+# Whitelist permitted frontend origins
+if defined? Rack::Cors
+  Rails.configuration.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins("https://#{ENV['DEFAULT_HOST']}")
+      resource '/assets/*', headers: :any, methods: %i[get head options]
+      resource '/packs/*', headers: :any, methods: %i[get head options]
+    end
+  end
+end
