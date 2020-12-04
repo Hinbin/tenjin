548680fcefbcd0670d96fdec352d904bb6e81683
Hinbin <nicholashoulton@gmail.com>
2020-12-04 08:30:18 +0000

    Small screen navigation fix

--- app/javascript/packs/controllers/dashboard_controller.js
+++ app/javascript/packs/controllers/dashboard_controller.js
@@ -7,6 +7,28 @@ export default class extends Controller {
     if (document.getElementById('oAuthEmail')) {
 
       const tour = new Shepherd.Tour({ useModalOverlay: true })
+
+      if (document.getElementById('current_user').offsetParent === null) {
+        tour.addStep({
+          id: 'Click Menu',
+          text: 'To get started, click here to expand the menu',
+          attachTo: {
+            element: '.navbar-toggler',
+            on: 'bottom'
+          },
+          buttons: [
+            {
+              text: 'Next',
+              action: tour.next
+            },
+            {
+              text: 'Later',
+              action: tour.cancel
+            }
+          ]
+        })
+      }
+
       tour.addStep({
         id: 'Link to Google',
         text: "Let's get your account linked to Google, so you don't have remember another username and password!  Click on your name",
