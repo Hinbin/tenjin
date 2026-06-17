82cbe3b8181c2d6e1eb660b0a65870739723c8fe
Hinbin <nicholashoulton@gmail.com>
2026-06-17 21:35:00 +0100

    Add Solid Queue plugin to Puma and create Render blueprint for test deployment

--- config/puma.rb
+++ config/puma.rb
@@ -36,5 +36,10 @@ preload_app!
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
+# Optionally run the Solid Queue worker inside the Puma process instead of as a
+# separate service. Set SOLID_QUEUE_IN_PUMA=true to enable (used by the free
+# single-service test deploy; production runs a dedicated worker instead).
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"].present?
+
 # Prevent timeouts in development mode
 worker_timeout (24*60*60) if ENV['RAILS_ENV']=='development'
