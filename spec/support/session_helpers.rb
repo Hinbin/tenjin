c04e77e3bf40f2e6f1fb36707e9ec50e9c716e67
Hinbin <nicholashoulton@gmail.com>
2026-06-19 21:03:15 +0100

    fix: update quiz start message in session helpers

--- spec/support/session_helpers.rb
+++ spec/support/session_helpers.rb
@@ -4,7 +4,7 @@ module SessionHelpers
   def log_in
     sign_in student
     visit root_path
-    expect(page).to have_text('START A QUIZ')
+    expect(page).to have_text('PRACTICE')
   end
 
   def stub_omniauth # rubocop:disable Metrics/MethodLength
